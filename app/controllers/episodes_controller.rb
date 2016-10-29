3fd23334405e4dadb31e97199b3cca8efc0e2085
Daniel Cardenas <dan85.cardenas@gmail.com>
2016-10-28 21:17:09 -0400

    Added styling to buttons

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -1,5 +1,5 @@
 class EpisodesController < ApplicationController
-  before_action :authenticate_admin!
+  before_action :authenticate_admin!, only: [:edit, :update, :destroy]
   before_action :set_episode, only: [:show, :edit, :update, :destroy]
   before_action :find_episode, only: [:show, :edit, :update, :destroy]
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :admins
   resources :episodes
-  root 'home#index'
+  root 'episodes#index'
 end
